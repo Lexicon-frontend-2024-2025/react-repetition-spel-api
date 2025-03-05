86e2a82b373a423556584ded546bf9e9c7300cf6
majazocom <maja.gedda@zocom.se>
2025-03-05 12:05:30 +0100

    render popular games

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
-import Image from "next/image";
+import { Game } from "@/interfaces";
 import styles from "./page.module.css";
+import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  // hämta populära spel från API
+  const res = await fetch(`https://api.rawg.io/api/games?key=bc1c2e51bf4d42acab01e84969eb1429`);
+  const data = await res.json();
+  console.log(data);
+
+  // få enbart de 6 första spelen
+  const popularGames: Game[] = data.results.slice(0, 6);
+
   return (
     <main className={styles.main}>
-      <h1>Spelsidan</h1>
+      <h1>Populära spel just nu</h1>
+      <section>
+        {popularGames.map((game) => (
+          <li key={game.id}>
+            {/* <Image
+              src={game.background_image}
+              alt={game.name}
+              width={100}
+              height={100}
+            /> */}
+            <p>{game.name}</p>
+          </li>
+        ))}
+      </section>
     </main>
   );
 }
---
+++ interfaces.ts
@@ -0,0 +1,5 @@
+export interface Game {
+    id: number,
+    background_image: string,
+    name: string
+}
